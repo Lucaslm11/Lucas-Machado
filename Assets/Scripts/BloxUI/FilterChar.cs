fee1d6f3686502baf0ee1503a073923cd10d5a73
abeltenera <abeltenera@gmail.com>
2020-03-15 17:52:27 +0000

    Add character filter to inputs

---
+++ Assets/Scripts/BloxUI/FilterChar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// FilterChar helps filter keyboard input on InputFields. Only characters matching the regular expression 
+/// are accepted.
+/// </summary>
+public class FilterChar : MonoBehaviour
+{
+    InputField InputField;
+    Regex regex;
+    [SerializeField] string RemoveMatchingRegex = @"[^a-zA-Z\d:]";
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputField = GetComponent<InputField>();
+        
+        regex = new Regex(RemoveMatchingRegex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If anykey was pressed, filters the text
+        if(Input.anyKey || Input.anyKeyDown)
+            InputField.text = Regex.Replace(InputField.text, RemoveMatchingRegex, "");
+    }
+
+  
+}
