171ff7b93c49a8617d1603b503acc9951367b24f
abeltenera <abeltenera@gmail.com>
2020-03-15 14:56:31 +0000

    Add dragging to blox

---
+++ Assets/Scripts/BloxUI/Blox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+/// <summary>
+/// Based on https://docs.unity3d.com/2018.1/Documentation/ScriptReference/EventSystems.IDragHandler.html
+/// </summary>
+public abstract class Blox : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler 
+{
+    private RectTransform bloxTransform;
+
+
+   
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        bloxTransform = GetComponent<RectTransform>();
+        print("Begin Dragging"); 
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        bloxTransform.position = eventData.position;
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        print("End draggin");
+    }
+
+
+}
---
+++ Assets/Scripts/BloxUI/IfBlox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfBlox : Blox
+{
+
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("HIIII");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
