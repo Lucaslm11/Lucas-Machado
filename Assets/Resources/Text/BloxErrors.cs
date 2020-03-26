fd3a9501361d33bd9622e86b6d0a54e56705e76a
abeltenera <abeltenera@gmail.com>
2020-03-26 17:21:35 +0000

    Add text resources

---
+++ Assets/Resources/Text/BloxErrors.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloxErrors
+{
+
+    public const string INT_BLOX_NO_VALUE = "O bloco Int é de preenchimento obrigatório";
+    public const string INT_BLOX_NO_NAME = "O bloco Int é de preenchimento obrigatório";
+    public const string BLOX_REPEATED_NAME = "Já existe no mesmo scope um bloco com o mesmo nome";
+}
