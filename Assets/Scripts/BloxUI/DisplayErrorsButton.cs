284e30d34e98ac0d21cf12be476aa1dafe569d99
abeltenera <abeltenera@gmail.com>
2020-03-26 15:46:28 +0000

    Add unadded files

---
+++ Assets/Scripts/BloxUI/DisplayErrorsButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayErrorsButton : MonoBehaviour
+{
+    private bool DisplayWindow = false;
+    [SerializeField] ErrorPopup ErrorPopup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ErrorPopup.gameObject.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchDisplayState()
+    {
+        DisplayWindow = !DisplayWindow;
+        ErrorPopup.gameObject.SetActive(DisplayWindow);
+    }
+
+
+}
