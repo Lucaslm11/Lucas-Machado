12247268d00d254fa5ce2b30185d0eb91a1975c4
abeltenera <abeltenera@gmail.com>
2020-03-26 22:04:35 +0000

    Add unadded files

---
+++ Assets/Scripts/BloxUI/ErrorMessage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using static HighlightableButton;
+
+public class ErrorMessage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] Text TextMessageOutput;
+    public BloxValidationError Error { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    public void SetTextMessage(string message)
+    {
+        TextMessageOutput.text = message;
+    }
+
+    public void SetErrorMessage(BloxValidationError error)
+    {
+        Error = error;
+        SetTextMessage(error.ErrorMessage);
+
+
+    }
+
+    public void HighlightTarget(ButtonHighlight highlight)
+    {
+        if (Error.TargetBlox != null && GameObjectHelper.HasComponent<HighlightableButton>(Error.TargetBlox.gameObject))
+        {
+            HighlightableButton hBtn = Error.TargetBlox.GetComponent<HighlightableButton>();
+            hBtn.HighlightButton(highlight);
+        }
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        HighlightTarget(ButtonHighlight.Error);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HighlightTarget(ButtonHighlight.None);
+    }
+}
---
+++ Assets/Scripts/BloxUI/HighlightableButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This component helps changing the normal color of a button
+/// Currently used when user puts it's mouse over an error message
+/// </summary>
+public class HighlightableButton : MonoBehaviour
+{
+    Dictionary<ButtonHighlight, Color> ColorDictionary;
+    public enum ButtonHighlight
+    {
+        // No highlight shall be applied
+        None,
+        // A success highlight shall be applied
+        Success,
+        // A warning highlight shall be applied
+        Warning,
+        // An error highlight shall be applied
+        Error,
+        // An info highlight shall be applied
+        Info
+    }
+        
+    [SerializeField] Button Button; 
+    [SerializeField] Color None = Color.white; 
+    [SerializeField] Color Success = Color.green; 
+    [SerializeField] Color Warning = Color.yellow; 
+    [SerializeField] Color Error = Color.red; 
+    [SerializeField] Color Info = Color.blue; 
+     
+    // Start is called before the first frame update
+    void Start()
+    { 
+        ColorDictionary = new Dictionary<ButtonHighlight, Color>();
+        ColorDictionary[ButtonHighlight.None] = None;
+        ColorDictionary[ButtonHighlight.Success] = Success;
+        ColorDictionary[ButtonHighlight.Warning] = Warning;
+        ColorDictionary[ButtonHighlight.Error] = Error;
+        ColorDictionary[ButtonHighlight.Info] = Info;
+
+        //HighlightButton(ButtonHighlight.None);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HighlightButton(ButtonHighlight bh)
+    {
+        ColorBlock cb = Button.colors;
+        cb.normalColor = ColorDictionary[bh];
+        Button.colors = cb;
+        
+    }
+}
