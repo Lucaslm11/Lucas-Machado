b97c20b4867917d2f76e79b428e600cdb262a6a1
abeltenera <abeltenera@gmail.com>
2020-03-28 22:25:01 +0000

    Add walk forward blox

---
+++ Assets/Scripts/Terminal/Nodes/FunctionTypes/VoidFunctionNode.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Scripts.Terminal.Nodes.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Terminal.Nodes.FunctionTypes
+{
+    public class VoidFunctionNode : CodeNode
+    {
+        public VoidFunctionNode( ) { 
+             
+        }
+
+
+        public override bool OnBeforeChildNodesExecuteAction()
+        {
+            Function();
+            return false;
+        }
+
+        /// <summary>
+        /// The different overrides of this will implement what is needed to execute a functions
+        /// </summary>
+        public virtual void Function()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Terminal/Nodes/Functions/ActionExecutorNode.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Terminal.Nodes.FunctionTypes;
+using Assets.Scripts.Terminal.Nodes.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Terminal.Nodes.Functions
+{
+    public class ActionExecutorNode : BooleanFunctionNode
+    {
+        Func<bool> WaitWhileCondition;
+        Action TaskToExecute;
+
+        /// <summary>
+        /// Constructor for ActionExecutorNode
+        /// waitWhileCondition will be called "endlessly" until it returns false
+        /// </summary>
+        /// /// <param name="taskToExecute"> A task that is called to execute </param>
+        /// <param name="waitWhileCondition"> A function that is called to check if it is executed </param>
+        public ActionExecutorNode(Action taskToExecute,Func<bool> waitWhileCondition)
+        {
+            TaskToExecute = taskToExecute;
+            WaitWhileCondition = waitWhileCondition;
+        }
+
+        public override void Function()
+        {
+            TaskToExecute();
+            TaskHelper.WaitWhile(WaitWhileCondition).Wait();
+        }
+
+    }
+}
