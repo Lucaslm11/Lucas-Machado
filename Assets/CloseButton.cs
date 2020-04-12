1cbc126337a3b994b6bab56aa27c59d10e973669
abeltenera <abeltenera@gmail.com>
2020-04-12 21:15:47 +0100

    Add tutorial ui components

---
+++ Assets/CloseButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseButton : MonoBehaviour
+{
+    /// <summary>
+    /// Sets gameObject active state to false
+    /// </summary>
+    /// <param name="gameObject"></param>
+    public void Close(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
+    }
+}
